9d7784f452d8bbe51afbf725e7fea1754d07b963
Olivier Ng'weno Kigotho <olikigotho@gmail.com>
2021-02-16 15:32:20 -0500

    Finnaly got the mosrse code conversion function to work!!!

--- oscilloscope.c
+++ oscilloscope.c
@@ -4,196 +4,197 @@
 #include "hardware/gpio.h"//pico hardware library
 #include "pico/binary_info.h"// binary viewing library
 #include "string.h"// string operaations library
+#include <stdlib.h>
 /*
 Project Description: This is the beginning of a code running in C/C++ on the rapsberry pi pico
 */
 
-// this function returns the morse code signal
-char morse_code (char text[]) {
-	int len = strlen(text);// get the length of the string
-	char signal[]; // declare a string of the signal
-	char chrtr[]; //declare a string of the character
-	char command[]; // declare a command string
-	int cmnd_len; // declera a command string
+char *morse_code (char text[]) {// retrun a pointer to the code
+        char mcsignal[10000] = "1110101110101110000000"; // declare a string of the signal
+        char command[30]; // declare a command string
+        char* p;// make a pointer to the string character
+        int i = 0; // declare an index
 
-	// loop throught the text and make a vector
-	for (int i = 0; i <= len; i++ ){
-		chrtr = text[i]; //character in the text
+	int clen;
+	int len = strlen(text);
+	int siglen;
+        // loop throught the text and make a vector
+        for (i = 0; i < len; i++ ){
+                //encode the signal into binary string
+                if (text[i] == 'A'){
+                                strcpy(command, "10111000");
+                } else  if (text[i] == 'a'){
+                                strcpy(command, "10111000");
+                } else  if (text[i] == 'B'){
+                                strcpy(command, "111010101000");
+                } else  if (text[i] == 'b'){
+                                strcpy(command, "111010101000");
+                } else  if (text[i] == 'C'){
+                                strcpy(command, "11101011101000");
+                } else  if (text[i] == 'c'){
+                                strcpy(command, "11101011101000");
+                } else  if (text[i] == 'D'){
+                                strcpy(command, "1110101000");
+                } else  if (text[i] == 'd'){
+                                strcpy(command, "1110101000");
+                } else  if (text[i] == 'E'){
+                                strcpy(command, "1000");
+                } else  if (text[i] == 'e'){
+                                strcpy(command, "1000");
+                } else  if (text[i] == 'F'){
+                                strcpy(command, "101011101000");
+                } else  if (text[i] == 'f'){
+                                strcpy(command, "101011101000");
+                } else  if (text[i] == 'G'){
+                                strcpy(command, "111011101000");
+                } else  if (text[i] == 'g'){
+                                strcpy(command, "111011101000");
+                } else  if (text[i] == 'H'){
+                                strcpy(command, "1010101000");
+                } else  if (text[i] == 'h'){
+                                strcpy(command, "1010101000");
+                } else  if (text[i] == 'I'){
+                                strcpy(command, "101000");
+                } else  if (text[i] == 'i'){
+                                strcpy(command, "101000");
+                } else  if (text[i] == 'J'){
+                                strcpy(command, "1011101110111000");
+                } else  if (text[i] == 'j'){
+                                strcpy(command, "1011101110111000");
+                } else  if (text[i] == 'K'){
+                                strcpy(command, "111010111000");
+                } else  if (text[i] == 'k'){
+                                strcpy(command, "111010111000");
+                } else  if (text[i] == 'L'){
+                                strcpy(command, "101110101000");
+                } else  if (text[i] == 'l'){
+                                strcpy(command, "101110101000");
+                } else  if (text[i] == 'M'){
+                                strcpy(command, "1110111000");
+                } else  if (text[i] == 'm'){
+                                strcpy(command, "1110111000");
+                } else  if (text[i] == 'N'){
+                                strcpy(command, "11101000");
+                } else  if (text[i] == 'n'){
+                                strcpy(command, "11101000");
+                } else  if (text[i] == 'O'){
+                                strcpy(command, "11101110111000");
+                } else  if (text[i] == 'o'){
+                                strcpy(command, "11101110111000");
+                } else  if (text[i] == 'P'){
+                                strcpy(command, "10111011101000");
+                } else  if (text[i] == 'p'){
+                                strcpy(command, "10111011101000");
+                } else  if (text[i] == 'Q'){
+                                strcpy(command, "1110111010111000");
+                } else  if (text[i] == 'q'){
+                                strcpy(command, "1110111010111000");
+                } else  if (text[i] == 'R'){
+                                strcpy(command, "1011101000");
+                } else  if (text[i] == 'r'){
+                                strcpy(command, "1011101000");
+                } else  if (text[i] == 'S'){
+                                strcpy(command, "10101000");
+                } else  if (text[i] == 's'){
+                                strcpy(command, "10101000");
+                } else  if (text[i] == 'T'){
+                                strcpy(command, "111000");
+                } else  if (text[i] == 't'){
+                                strcpy(command, "111000");
+                } else  if (text[i] == 'U'){
+                                strcpy(command, "1010111000");
+                } else  if (text[i] == 'u'){
+                                strcpy(command, "1010111000");
+                } else  if (text[i] == 'V'){
+                                strcpy(command, "101010111000");
+                } else  if (text[i] == 'v'){
+                                strcpy(command, "101010111000");
+                } else  if (text[i] == 'W'){
+                                strcpy(command, "101110111000");
+                } else  if (text[i] == 'w'){
+                                strcpy(command, "101110111000");
+                } else  if (text[i] == 'X'){
+                                strcpy(command, "11101010111000");
+                } else  if (text[i] == 'x'){
+                                strcpy(command, "11101010111000");
+                } else  if (text[i] == 'Y'){
+                                strcpy(command, "1110101110111000");
+                } else  if (text[i] == 'y'){
+                                strcpy(command, "1110101110111000");
+                } else  if (text[i] == 'Z'){
+                                strcpy(command, "11101110101000");
+                } else  if (text[i] == 'z'){
+                                strcpy(command, "11101110101000");
+                } else  if (text[i] == '0'){
+                                strcpy(command, "1110111011101110111000");
+                } else  if (text[i] == '1'){
+                                strcpy(command, "10111011101110111000");
+                } else  if (text[i] == '2'){
+                                strcpy(command, "101011101110111000");
+                } else  if (text[i] == '3'){
+                                strcpy(command, "1010101110111000");
+                } else  if (text[i] == '4'){
+                                strcpy(command, "10101010111000");
+                } else  if (text[i] == '5'){
+                                strcpy(command, "101010101000");
+                } else  if (text[i] == '6'){
+                                strcpy(command, "11101010101000");
+                } else  if (text[i] == '7'){
+                                strcpy(command, "1110111010101000");
+                } else  if (text[i] == '8'){
+                                strcpy(command, "111011101110101000");
+                } else  if (text[i] == '9'){
+                                strcpy(command, "11101110111011101000");
+                } else  if (text[i] == '.'){
+                                strcpy(command, "10111010111010111000");
+                } else  if (text[i] == ','){
+                                strcpy(command, "1110111010101110111000");
+                } else  if (text[i] == '?'){
+                                strcpy(command, "101011101110101000");
+                } else  if (text[i] == '\''){
+                                strcpy(command, "1011101110111011101000");
+                } else  if (text[i] == '!'){
+                                strcpy(command, "1110101110101110111000");
+                } else  if (text[i] == '/'){
+                                strcpy(command, "1110101011101000");
+                } else  if (text[i] == '('){
+                                strcpy(command, "1110101011101000");
+                } else  if (text[i] == ')'){
+                                strcpy(command, "1110101110111010111000");
+                } else  if (text[i] == '&'){
+                                strcpy(command, "10111010101000");
+                } else  if (text[i] == ':'){
+                                strcpy(command, "11101110111010101000");
+                } else  if (text[i] == ';'){
+                                strcpy(command, "11101011101011101000");
+                } else  if (text[i] == '='){
+                                strcpy(command, "1110101010111000");
+                } else  if (text[i] == '+'){
+                                strcpy(command, "1011101011101000");
+                } else  if (text[i] == '-'){
+                                strcpy(command, "111010101010111000");
+                } else  if (text[i] == '_'){
+                                strcpy(command, "10101110111010111000");
+                } else  if (text[i] == '"'){
+                                strcpy(command, "101110101011101000");
+                } else  if (text[i] == '$'){
+                                strcpy(command, "10101011101010111000");
+                } else  if (text[i] == '@'){
+                                strcpy(command, "10111011101011101000");
+                } else  if (text[i] == ' '){
+                                strcpy(command, "0000");
+                } else {
+                                strcpy(command, "101010101010101000");
 
-		//encode the signal into binary strings
-		switch (chrtr){
-			case 'A' :
-				command = '10111000';
-                        case 'a' :
-                                command = '10111000';
-                        case 'B' :
-                                command = '111010101000';
-                        case 'b' :
-                                command = '111010101000';
-                        case 'C' :
-                                command = '11101011101000';
-                        case 'c' :
-                                command = '111010101000';
-                        case 'D' :
-                                command = '1110101000';
-                        case 'd' :
-                                command = '1110101000';
-			case 'E' :
-				command = '1000';
-                        case 'e' :
-                                command = '1000';
-			case 'F' :
-				command = '101011101000;
-                        case 'f' :
-                                command = '101011101000;
-			case 'G' :
-				command = '111011101000';
-			case 'g' :
-				command = '111011101000'
-			case 'H' :
-				command = '1010101000';
-			case 'h' :
-				command = '1010101000';
-			case 'I' :
-				command = '101000';
-			case 'i' :
-				command = '101000';
-			case 'J' :
-				command = '1011101110111000';
-			case 'j' :
-				command = '1011101110111000';
-			case 'K' :
-				command = '111010111000';
-			case 'k' :
-				command = '111010111000';
-			case 'L' :
-				command = '101110101000';
-			case 'l' :
-				command = '101110101000';
-			case 'M' :
-				command = '1110111000';
-			case 'm' :
-				command = '1110111000';
-			case 'N' :
-				command = '11101000';
-			case 'n' :
-				command = '11101000';
-			case 'O' :
-				command = '11101110111000';
-			case 'o' :
-				command = '11101110111000';
-			case 'P' :
-				command = '10111011101000';
-			case 'p' :
-				command = '10111011101000';
-			case 'Q' :
-				command = '1110111010111000';
-			case 'q' :
-				command = '1110111010111000';
-			case 'R' :
-				command = '1011101000';
-			case 'r' :
-				command = '1011101000';
-			case 'S' :
-				command = '10101000';
-			case 's' :
-				command = '10101000';
-			case 'T' :
-				command = '111000';
-			case 't' :
-				command = '111000';
-			case 'U' :
-				command = '1010111000';
-			case 'u' :
-				command = '1010111000';
-			case 'V' :
-				command = '101010111000';
-			case 'v' :
-				command = '101010111000';
-			case 'W' :
-				command = '101110111000';
-			case 'w' :
-				command = '101110111000';
-			case 'X' :
-				command = '11101010111000';
-			case 'x' :
-				command = '11101010111000';
-			case 'Y' :
-				command = '1110101110111000';
-			case 'y' :
-				command = '1110101110111000';
-			case 'Z' :
-				command = '11101110101000';
-			case 'z' :
-				command = '11101110101000';
-			case '0' :
-				command = '1110111011101110111000';
-			case '1' :
-				command = '10111011101110111000';
-			case '2' :
-				command = '101011101110111000';
-			case '3' :
-				command = '1010101110111000;
-			case '4' :
-				command = '10101010111000';
-			case '5' :
-				command = '101010101000';
-			case '6' :
-				command = '11101010101000';
-			case '7' :
-				command = '1110111010101000';
-			case '8' :
-				command = '111011101110101000';
-			case '9' :
-				command = '11101110111011101000';
-			case '.' :
-				command = '10111010111010111000';
-			case ',' :
-				command = '1110111010101110111000';
-			case '?' :
-				command = '101011101110101000';
-			case '\'' :
-				command = '1011101110111011101000';
-			case '!' :
-				command = '1110101110101110111000';
-			case '/' :
-				command = '1110101011101000';
-			case '(' :
-				command = '1110101011101000';
-			case ')' :
-				command = '1110101110111010111000';
-			case '&' :
-				command = '10111010101000';
-			case ':' :
-				command = '11101110111010101000';
-			case ';' :
-				command = '11101011101011101000';
-			case '=' :
-				command = '1110101010111000';
-			case '+' :
-				command = '1011101011101000';
-			case '-' :
-				command = '111010101010111000';
-			case '_' :
-				command = '10101110111010111000';
-			case '"' :
-				command = '101110101011101000';
-			case '$' :
-				command = '10101011101010111000';
-			case '@' :
-				command = '10111011101011101000';
-			case ' ' :
-				command = '0000';
-			default  :
-				command = '101010101010101000';
-		// measure the length of the command
-		cmnd_len = strlen(command);
-
-		//add the command to the signal
-		strncat(signal, command, cmnd_len);
-		}
-	return signal;
-	}
+                //add the command to the signal
+                 // test print the command
+                }
+		clen = strlen(command);
+		strncat(mcsignal, command, clen);
+        }
+        char *code = malloc(strlen(mcsignal));
+        strcpy(code,mcsignal);
+        return code;
 }
 
 
@@ -228,32 +229,12 @@ int main() {
 
 	// initialize the level of the game
 	int level = 0;
-
+	char *h;
 	// While loop for the system to excute. 1 indicates the bool true
 	while (1) {
-		if (level == 0) {  // level 1
-			gpio_put(LED_PIN, 1); // turn on LED
-			sleep_ms(100); // sleep for 0.1s
-			gpio_put(LED_PIN, 0); //turn ongpio_put(PIN_10, 0); // turn off pin 10
-                        
-			sleep_ms(100); // sleep for 0.1s
-
-                        gpio_put(PIN_10, 1); // turn off pin 10
-                        sleep_ms(100); // sleep for 0.1s
-                        gpio_put(PIN_10, 0); //turn ongpio_put(PIN_10, 0); // t$
-                        sleep_ms(100); // sleep for 0.1s
-
-		} else if (level == 1){  // level 2
-			gpio_put(PIN_10, 0); // turn off pin 10
-                        sleep_ms(100); // sleep for 0.1s
-                        gpio_put(PIN_10, 1); //turn on pin 10
-                        sleep_ms(100); // slep for 0.1s
-
-		} else {  // level 3
-			gpio_put(PIN_10, 0); // turn off pin 10
-                        sleep_ms(100); // sleep for 0.1s
-                        gpio_put(PIN_10, 1); //turn on pin 10
-                        sleep_ms(100); // slep for 0.1s
-		}
+		h = morse_code("Hello World K");
+		free(h);
+		printf("%s\n", h);
+		sleep_ms(1000);
 	}
 }
